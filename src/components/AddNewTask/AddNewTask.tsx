2fce0e4367ae340eee2d3d4647e058d7fc3f36b7
Marchenko, Vadim3 <v3.marchenko@itransition.com>
2020-10-26 11:49:39 +0300

    added save new task to localstorage

--- src/components/AddNewTask/AddNewTask.tsx
+++ src/components/AddNewTask/AddNewTask.tsx
@@ -6,10 +6,26 @@ const AddNewTask = (): JSX.Element => {
 
   const onLabelChange = (e: React.ChangeEvent<HTMLInputElement>): void => setTaskDescription(e.target.value);
 
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    if (taskDescription) saveTask(taskDescription);
+  };
+
+  const saveTask = (taskDescription: string): void => {
+    const arrayOfTasks: Array<string> = JSON.parse(localStorage.getItem('arrayOfTasks'))
+      ? JSON.parse(localStorage.getItem('arrayOfTasks'))
+      : [];
+
+    arrayOfTasks.push(taskDescription);
+    localStorage.setItem('arrayOfTasks', JSON.stringify(arrayOfTasks));
+    setTaskDescription('');
+  };
+
   return (
     <Container>
       <Typography variant="h5">Add new task</Typography>
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid>
           <TextField variant="outlined" label="Write your task" value={taskDescription} onChange={onLabelChange} />
         </Grid>
