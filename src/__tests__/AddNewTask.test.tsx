5096e2d17bfd4ab0c7ae79f3402edc66ac39e9f5
chakapega <chakapega@gmail.com>
2020-10-31 21:39:31 +0300

    added the form submit event test

--- src/__tests__/AddNewTask.test.tsx
+++ src/__tests__/AddNewTask.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { fireEvent, render } from '@testing-library/react';
 import AddNewTask from '../components/AddNewTask';
+import taskService from '../services';
+
+jest.mock('../services/index.ts', () => ({ addTask: jest.fn() }));
 
 const renderComponent = () => render(<AddNewTask />);
 
@@ -41,12 +44,19 @@ describe('AddNewTask', () => {
     expect(button).toHaveTextContent('Add');
   });
 
-  it('check Button disabled attribute', () => {
+  it('checks the Button disabled attribute', () => {
     const { container } = renderComponent();
     const button = container.querySelector('button');
 
     expect(button.disabled).toBe(true);
     fireEvent.change(container.querySelector('input'), { target: { value: 'writed text' } });
     expect(button.disabled).toBe(false);
+  });
+
+  it('checks the form submit event', () => {
+    const { container } = renderComponent();
+
+    fireEvent.submit(container.querySelector('form'));
+    expect(taskService.addTask).toHaveBeenCalled();
   });
 });
