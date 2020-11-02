545d4e8ecd99eff48359d1885e22e672d66feef5
Marchenko, Vadim3 <v3.marchenko@itransition.com>
2020-11-02 18:40:44 +0300

    added express, typescript, http-status-codes packages

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const taskRouter = require('./resources/task/task.router');
+
+app.use(express.json());
+
+app.use("/api", taskRouter);
+
+app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
